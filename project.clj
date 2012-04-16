80a18af04dd584e8c09267f151088a58fd0d1f03
Cosmin Stejerean <cosmin@offbytwo.com>
2012-04-15 22:07:11 -0700

    switching to IClojure 1.1 and Clojure 1.4.0

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
-0;95;c
 (defproject lein-iclojure "1.0.0-SNAPSHOT"
-  :dependencies [[com.offbytwo.iclojure/iclojure "1.0.2-SNAPSHOT" :exclusions [org.clojure/clojure]]]
-  :dev-dependencies [[org.clojure/clojure "1.3.0"]]
+  :dependencies [[com.offbytwo.iclojure/iclojure "1.1.0" :exclusions [org.clojure/clojure]]]
+  :dev-dependencies [[org.clojure/clojure "1.4.0"]]
   :description "Launch an IClojure REPL"
   :eval-in-leiningen true
   )
