a7e7630755664e167798896c0d5c55a9c25e0536
Cosmin Stejerean <cosmin@offbytwo.com>
2012-04-15 22:08:01 -0700

    prepare release lein-iclojure 1.0

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject lein-iclojure "1.0.0-SNAPSHOT"
+(defproject lein-iclojure "1.0"
   :dependencies [[com.offbytwo.iclojure/iclojure "1.1.0" :exclusions [org.clojure/clojure]]]
-  :dev-dependencies [[org.clojure/clojure "1.4.0"]]
+  :dev-dependencies [[org.clojure/clojure "1.4.0"]
+                     [lein-clojars "0.6.0"]]
   :description "Launch an IClojure REPL"
-  :eval-in-leiningen true
-  )
+  :eval-in-leiningen true)
