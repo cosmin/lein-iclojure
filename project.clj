8727e6ebda41e306748eb3069f559d95045b0347
Cosmin Stejerean <cosmin@offbytwo.com>
2013-05-10 17:59:37 -1000

    prepare for next development iteration

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-iclojure "1.2"
+(defproject lein-iclojure "1.3-SNAPSHOT"
   :dependencies [[com.offbytwo.iclojure/iclojure "1.2.0" :exclusions [org.clojure/clojure]]]
   :description "Launch an IClojure REPL"
   :eval-in-leiningen true)
